3c65826df59d7886fd89e86b92a360559708017d
tanvir hasan <tanvir002700@gmail.com>
2020-02-09 22:33:04 +0600

    add pi fan control launcher script

--- control-fan.py
+++ control-fan.py
@@ -31,17 +31,16 @@ def handleFan():
     CPU_temp = float(getCPUtemperature())
     if CPU_temp > maxTMP:
         fanON()
-        print("fan on")
+        #print("fan on")
         sleep(30)
     if CPU_temp <= maxTMP-10:
         fanOFF()
-        print("fan off")
+        #print("fan off")
     return
 
 try:
     setup()
     while True:
-        print("running", getCPUtemperature())
         handleFan()
         sleep(5) # Read the temperature every 5 sec
 except KeyboardInterrupt: # trap a CTRL+C keyboard interrupt
---
+++ launcher.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# launcher.sh
+
+sudo python /etc/control-pi/fan_shutdown.py &
